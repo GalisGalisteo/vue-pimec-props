20d152b16b277fdd4da384ab63451725625c8597
GalisGalisteo <galis.travelling@gmail.com>
2023-11-25 19:03:25 +0100

    update

--- src/components/WeddingInvitation.vue
+++ src/components/WeddingInvitation.vue
@@ -1,28 +1,29 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
+defineProps(["name"]);
+defineEmits(["with-partner"]);
 
-defineProps(['name'])
-defineEmits(['with-partner'])
-
-const withPartner = ref(false)
-
-
-
+const withPartner = ref(false);
 </script>
 
 <template>
-    <div class="container">
-        <h2> Invitado : {{ name }}</h2>
-        <label for="partner">¿Vendrás con acomapañante?</label>
-        <input @change="$emit('withPartner', withPartner, name)" v-model="withPartner" type="checkbox" id="partner">
-    </div>
+  <div class="container">
+    <h2>Invitado : {{ name }}</h2>
+    <label for="partner">¿Vendrás con acomapañante?</label>
+    <input
+      @change="$emit('withPartner', withPartner, name)"
+      v-model="withPartner"
+      type="checkbox"
+      id="partner"
+    />
+  </div>
 </template>
 
 <style scoped>
 .container {
-    width: 200px;
-    border: 1px solid black;
-    padding: 1rem;
+  width: 200px;
+  border: 1px solid black;
+  padding: 1rem;
 }
 </style>
